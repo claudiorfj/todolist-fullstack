85c412a359fa7e8772b7306fc3b46bf6e1684942
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-03 16:05:05 +0100

    create  validateFieldStatus

--- backend/src/middlewares/tasksMiddleware.js
+++ backend/src/middlewares/tasksMiddleware.js
@@ -1,19 +1,29 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
 /* eslint-disable linebreak-style */
-const validateBody = (req, res, next) => {
+const validateFieldTitle = (req, res, next) => {
   const { body } = req
-
   if(body.title === undefined){
     return res.status(400).json({ message: 'The field "title" is required' })
   }
-
   if(body.title === ''){
     return res.status(400).json({ message: 'The field "title" cannot be empty' })
   }
   next()
 }
 
+const validateFieldStatus = (req, res, next) => {
+  const { body } = req
+  if(body.status === undefined){
+    return res.status(400).json({ message: 'The field "status" is required' })
+  }
+  if(body.status === ''){
+    return res.status(400).json({ message: 'The field "status" cannot be empty' })
+  }
+  next()
+}
+
 module.exports = {
-  validateBody,
+  validateFieldTitle,
+  validateFieldStatus,
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -5,8 +5,8 @@ const tasksController = require('./controllers/taskController')
 const tasksMiddleware = require('./middlewares/tasksMiddleware')
 
 router.get('/tasks', tasksController.getAll)
-router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask)
+router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask)
 router.delete('/tasks/:id', tasksController.deleteTask)
-router.put('/tasks/:id', tasksController.updateTask)
+router.put('/tasks/:id', tasksMiddleware.validateFieldStatus, tasksMiddleware.validateFieldTitle,tasksController.updateTask)
 
 module.exports = router
