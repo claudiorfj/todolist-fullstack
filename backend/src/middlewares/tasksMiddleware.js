f855a119e9cecda0b32941c0fef3370df90eeb00
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-03 15:41:07 +0100

    Create tasksMiddleware.js

---
+++ backend/src/middlewares/tasksMiddleware.js
@@ -0,0 +1,19 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+const validateBody = (req, res, next) => {
+  const { body } = req
+
+  if(body.title === undefined){
+    return res.status(400).json({ message: 'The field "title" is required' })
+  }
+
+  if(body.title === ''){
+    return res.status(400).json({ message: 'The field "title" cannot be empty' })
+  }
+  next()
+}
+
+module.exports = {
+  validateBody,
+}
