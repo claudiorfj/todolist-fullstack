76ef0425a9c05ae7b6a744f9dd38b4e6673142b9
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-03 15:41:45 +0100

    add taskMiddleware

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,10 @@
 /* eslint-disable linebreak-style */
 const express = require('express')
-const tasksController = require('./controllers/taskController')
 const router = express.Router()
+const tasksController = require('./controllers/taskController')
+const tasksMiddleware = require('./middlewares/tasksMiddleware')
 
 router.get('/tasks', tasksController.getAll)
-router.post('/tasks', tasksController.createTask)
+router.post('/tasks', tasksMiddleware, tasksController.createTask)
 
 module.exports = router
