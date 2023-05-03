3fdc7ed91bf7471b99d7d8715655233b5fdd6b32
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-03 16:09:46 +0100

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 /* eslint-disable linebreak-style */
 const express = require('express')
+const cors = require('cors')
 const router = require('./router')
 const app = express()
 
 app.use(router)
 app.use(express.json())
+app.use(cors())
 
 module.exports = app
