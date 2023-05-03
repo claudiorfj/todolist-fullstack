cca8cc91985035a34f53e74a824c258735eb4701
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-03 15:00:54 +0100

    create getAll  and createTask functions

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,9 +1,22 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable indent */
 /* eslint-disable linebreak-style */
-const getAll = () => {
+const connection = require('./connection')
 
+const getAll = async () => {
+    const [tasks] = await connection.execute('SELECT * FROM tasks')
+    return tasks
+}
+
+const createTask = async (task) => {
+    const { title } = task
+    const dateUTC = new Date(Date.now()).toUTCString()
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES(?, ?, ?)'
+    const createdTask = await connection.execute(query, [title, 'pendente', dateUTC])
+    return createdTask
 }
 
 module.exports = {
-    getAll
+    getAll,
+    createTask,
 }
